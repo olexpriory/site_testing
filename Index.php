e7176b19d063ef6cb32f3a2a3deaf2b3989c3acc
Rostislav <winfre20company@gmail.com>
2018-12-27 13:09:49 +0200

    xbsd

--- Index.php
+++ Index.php
@@ -60,8 +60,7 @@
         }elseif ($count == 1){
             $fmsg = "Ви один в системі!";
             $_SESSION['user_id'] = null;
-           // echo "<h1>username = $username </h1>";
-           // exit;
+
             $_SESSION['user_id'] = mysqli_fetch_array(mysqli_query($conection, "SELECT * FROM `users` WHERE username = '$username' and usersurname = '$usersurname'  "))['id_user'];
             header("Location: test_start.php"); 
             //include ('test_start.php'); 
